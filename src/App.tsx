48b333587555c4b1dea6bf30e52865bade5105e2
nahanul <ousuo9617@naver.com>
2024-07-11 21:34:05 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='Head'>
+      <div className="App">
+        <header>
+          <div className='sub-header-content'>
+            <div>@TODO Icons</div>
+            <div className='short-cut'>
+              <ul>
+                <li>HOME</li>
+                <li>SITE MAP</li>
+                <li>CONTACT US</li>
+              </ul>
+            </div>
+          </div>
+        </header>
+      </div>
+      <div className='gnb'>
+        <header>
+          <div className='sub-header-content'>
+            <div className='monami-gnb'>
+              <img src='http://www.monami.com/images/common/logo.jpg'></img>
+            </div>
+            <div className='nav'>
+              <ul>
+                <li>모나미소개</li>
+                <li>모나미 제품</li>
+                <li>NEWS & VIDEO</li>
+                <li>모나미 미술대회</li>
+                <li>고객지원</li>
+                <li>기업 구매 / 개발</li>
+              </ul>
+            </div>
+            <div className='search'>
+              <div className='nav_search'>
+              
+              </div>
+              <div className='btn_all'>
+
+              </div>
+            </div>
+          </div>
+        </header>
+      </div>
     </div>
   );
 }
