25ac5879a42e1143882aec323d661d2c79428d22
nahanul <ousuo9617@naver.com>
2024-07-11 23:13:55 +0900

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import SearchIcon from './icons/SearchIcon';
 
 function App() {
   return (
@@ -18,32 +19,30 @@ function App() {
           </div>
         </header>
       </div>
-      <div className='gnb'>
-        <header>
-          <div className='sub-header-content'>
-            <div className='monami-gnb'>
-              <img src='http://www.monami.com/images/common/logo.jpg'></img>
+      <div className='nav-wrapper'>
+        <div className='sub-header-content'>
+          <img src='http://www.monami.com/images/common/logo.jpg'
+            height={88}
+          />
+          <div className='nav'>
+            <ul>
+              <a href='#'><li>모나미소개</li></a>
+              <a href='#'><li>모나미 제품</li></a>
+              <a href='#'><li>NEWS & VIDEO</li></a>
+              <a href='#'><li>모나미 미술대회</li></a>
+              <a href='#'><li>고객지원</li></a>
+              <a href='#'><li>기업 구매 / 개발</li></a>
+            </ul>
+          </div>
+          <div className='sub-nav'>
+            <div className='nav_search'>
+              <SearchIcon className='icon' />
             </div>
-            <div className='nav'>
-              <ul>
-                <li>모나미소개</li>
-                <li>모나미 제품</li>
-                <li>NEWS & VIDEO</li>
-                <li>모나미 미술대회</li>
-                <li>고객지원</li>
-                <li>기업 구매 / 개발</li>
-              </ul>
-            </div>
-            <div className='search'>
-              <div className='nav_search'>
-              
-              </div>
-              <div className='btn_all'>
+            <div className='btn_all'>
 
-              </div>
             </div>
           </div>
-        </header>
+        </div>
       </div>
     </div>
   );
